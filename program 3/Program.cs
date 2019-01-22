0df2896ec2725b5b7ed9097de66a37f4d4d851e7
saksham rana <0blackbuck@gmail.com>
2019-01-22 18:23:19 -0500

    p1

--- program 3/Program.cs
+++ program 3/Program.cs
@@ -6,7 +6,28 @@ namespace program_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int val1, val2, val3;
+            Console.WriteLine("Please enter value of x");
+            val1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter value of y");
+            val2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter value of z");
+            val3 = Convert.ToInt32(Console.ReadLine());
+
+            if (val1 > val2 && val1 > val3)
+            {
+                Console.WriteLine("The greatest number is" +val1);
+            }
+            else if (val2 > val1 && val2 > val3)
+
+                Console.WriteLine("The greatest number is" +val2);
+
+            else
+            {
+                Console.WriteLine("The greatest number is" +val3);
+            }
+            Console.WriteLine("press enter to exit");
+            Console.ReadKey(true);
         }
     }
 }
